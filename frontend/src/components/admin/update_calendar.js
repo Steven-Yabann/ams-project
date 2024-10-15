87314c05033c4016525f9ac4e37bd6e190ba8dd3
Steven-Yabann <stevekiptoo1@gmail.com>
2024-10-15 18:55:00 +0300

    improved naming of folder for clarity

--- frontend/src/components/admin/update_calendar.js
+++ frontend/src/components/admin/update_calendar.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import FullCalendar from '@fullcalendar/react'; 
-import dayGridPlugin from '@fullcalendar/daygrid'; 
-import interactionPlugin from '@fullcalendar/interaction'; 
+import dayGridPlugin from '@fullcalendar/daygrid/index.js'; 
+import interactionPlugin from '@fullcalendar/interaction/index.js'; 
 import './css_files/update_calender.css';
 
 export default function UpdateCalender() {
