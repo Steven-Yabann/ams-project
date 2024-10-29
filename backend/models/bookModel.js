03f02bff93f9f47855113a24711032d768743257
Sharlet-Kirui <sharlet.kirui@gmail.com>
2024-10-29 16:05:29 +0300

    book schema

---
+++ backend/models/bookModel.js
@@ -0,0 +1,48 @@
+const mongoose = require('mongoose');
+
+const bookSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    author: {
+        type: String,
+        required: true
+    },
+    isbn: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    publisher: {
+        type: String,
+        required: true
+    },
+    yearPublished: {
+        type: Number,
+        required: true
+    },
+    category: {
+        type: String,
+        required: true
+    },
+    location: {
+        type: String,
+        required: true
+    },
+    totalCopies: {
+        type: Number,
+        required: true
+    },
+    copiesAvailable: {
+        type: Number,
+        required: true
+    },
+    status: {
+        type: String,
+        required: true,
+        enum: ['Available', 'Borrowed', 'Lost', 'Maintenance']
+    }
+});
+
+module.exports = mongoose.model('Book', bookSchema);
