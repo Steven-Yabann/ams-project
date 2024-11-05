282c34f8dea1fad98545e9bb9efc3b86a254cdd6
fak3unr3a1 <102730333+fak3unr3a1@users.noreply.github.com>
2024-11-05 14:27:19 +0300

    fixed spelling in dashboard:(

--- frontend/src/components/global/sidebar.js
+++ frontend/src/components/global/sidebar.js
@@ -10,7 +10,7 @@ export default function Sidebar({ user_cat }){
                     <>
                         <h2>ADMIN</h2>
                         <ul>
-                        <Link to="/admin_dashboard">Dasboard</Link>
+                        <Link to="/admin_dashboard">Dashboard</Link>
                         <Link to="/create-user">Create User</Link>
                         <Link to="/verify-fees">Pay Fees</Link>
                         <Link to="/update_calender">Update Calender</Link>
